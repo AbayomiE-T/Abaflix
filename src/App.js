68ee4c1956a7e6225a3ccbe762ab66c30b53d0bf
Abayomi <eyitouyoa3@gmail.com>
2022-03-23 11:30:36 -0400

    Added some routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import styles from './App.module.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Navigation/Header';
+import MovieList from './components/Movies/MovieList';
+import LikedMovies from './components/Movies/LikedMovies';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <Header></Header>
-    </div>
+    <Router>
+      <div className={styles.app}>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <MovieList />
+          </Route>
+          <Route path="/liked">
+            <LikedMovies />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
