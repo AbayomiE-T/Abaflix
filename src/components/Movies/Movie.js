e95e19033275d3604ecf7cb27bfa21ac570b2a6e
Abayomi <eyitouyoa3@gmail.com>
2022-03-23 23:10:50 -0400

    added scale on hover animation

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import style from './Movie.module.css';
+
 const baseUrl = 'https://image.tmdb.org/t/p/w300';
 
 const Movie = (props) => {
     return (
         <Link to={`/detail/${props.id}`} className={`${props.className}`}>
-            <img className={StyleSheet.poster} src={`${baseUrl}${props.src}`} alt={props.title} />
+            <img className={style.grow} src={`${baseUrl}${props.src}`} alt={props.title} />
         </Link>
     )
 }
