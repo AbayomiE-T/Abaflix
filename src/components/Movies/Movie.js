c88938ac3abc862cdeb11bb4c55782964282d066
Abayomi <eyitouyoa3@gmail.com>
2022-03-24 08:09:11 -0400

    grid is responsive

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -8,7 +8,7 @@ const baseUrl = 'https://image.tmdb.org/t/p/w300';
 const Movie = (props) => {
     return (
         <Link to={`/detail/${props.id}`} className={`${props.className}`}>
-            <img className={style.grow} src={`${baseUrl}${props.src}`} alt={props.title} />
+            <img className={`${style.grow} ${style.poster}`} src={`${baseUrl}${props.src}`} alt={props.title} />
         </Link>
     )
 }
